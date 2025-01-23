e49b11105579342845b64f5b8ce0ccb6ad9b5b1c
vdvhk12 <vdvhk12@gmail.com>
2025-01-24 03:12:03 +0900

    feat: 시큐리티 설정

---
+++ backend/src/main/java/com/chat/backend/global/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.chat.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
+			.build();
+	}
+}
