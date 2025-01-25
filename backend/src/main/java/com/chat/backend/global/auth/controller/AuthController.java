c0b0559f2aeafa207e4eaaf5a89b8ddcbde446ed
vdvhk12 <vdvhk12@gmail.com>
2025-01-26 01:43:34 +0900

    feat: 로그인

---
+++ backend/src/main/java/com/chat/backend/global/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.chat.backend.global.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chat.backend.domain.member.dto.MemberDto;
+import com.chat.backend.domain.member.entity.Member;
+import com.chat.backend.domain.member.repository.MemberRepository;
+import com.chat.backend.global.auth.dto.AuthForm;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final MemberRepository memberRepository;
+
+	@PostMapping("/login")
+	public ResponseEntity<MemberDto> login(@RequestBody AuthForm authForm) {
+		Member member = memberRepository.findByUsername(authForm.username()).orElse(null);
+		assert member != null;
+		return ResponseEntity.status(HttpStatus.OK).body(MemberDto.from(member));
+	}
+}
---
+++ backend/src/main/java/com/chat/backend/global/auth/dto/AuthForm.java
@@ -0,0 +1,4 @@
+package com.chat.backend.global.auth.dto;
+
+public record AuthForm (String username, String password) {
+}
