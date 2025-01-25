31ce5452c6fc8e5e3e0625a075ab18696ce228ad
vdvhk12 <vdvhk12@gmail.com>
2025-01-26 01:43:49 +0900

    feat: cors 설정 추가

--- backend/src/main/java/com/chat/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/chat/backend/global/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -16,7 +18,19 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
 			.build();
+	}
+
+	@Bean
+	UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("http://localhost:3000");  // 허용할 도메인
+		config.addAllowedMethod("*");  // 모든 HTTP 메서드 허용
+		config.addAllowedHeader("*");  // 모든 헤더 허용
+		source.registerCorsConfiguration("/**", config);  // 모든 경로에 CORS 설정 적용
+		return source;
 	}
 }
