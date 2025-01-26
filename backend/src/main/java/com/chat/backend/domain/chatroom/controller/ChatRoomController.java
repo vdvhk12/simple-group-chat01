687ab1060be7dc73173e6755ebc74eaa522246ec
vdvhk12 <vdvhk12@gmail.com>
2025-01-26 13:36:55 +0900

    refactor: 사용하지 않는 import 제거

--- backend/src/main/java/com/chat/backend/domain/chatroom/controller/ChatRoomController.java
+++ backend/src/main/java/com/chat/backend/domain/chatroom/controller/ChatRoomController.java
@@ -1,7 +1,5 @@
 package com.chat.backend.domain.chatroom.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
